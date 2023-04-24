2b768346801171c82321d4a6e2351839e2cd6dca
Jonas Flückiger <fluckiger.jonas.dev@gmail.com>
2023-04-24 14:08:22 +0200

    dummy tests

--- sources/Graphpp/Tests/tst_graphpptest.cpp
+++ sources/Graphpp/Tests/tst_graphpptest.cpp
@@ -2,6 +2,7 @@
 #include <QtTest>
 
 // add necessary includes here
+#include <graphpplib.h>
 
 class GraphppTest : public QObject
 {
@@ -13,6 +14,9 @@ public:
 
 private slots:
     void test_case1();
+    void test_case2();
+    void test_case3();
+    void test_case4();
 
 };
 
@@ -28,8 +32,23 @@ GraphppTest::~GraphppTest()
 
 void GraphppTest::test_case1()
 {
-
+    QVERIFY(1 == 1);
 }
+void GraphppTest::test_case2()
+{
+    QVERIFY(1 != 12);
+}
+
+void GraphppTest::test_case3()
+{
+    QVERIFY(1 > -1);
+}
+
+void GraphppTest::test_case4()
+{
+    QVERIFY(0 == 0);
+}
+
 
 QTEST_APPLESS_MAIN(GraphppTest)
 
